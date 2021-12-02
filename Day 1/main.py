fb0ccbc10780139607774f8fb96212c26c12519d
richwill28 <e0550368@u.nus.edu>
2021-12-03 03:43:44 +0800

    Feat(Day 1): Add day 1 solution

---
+++ Day 1/main.py
@@ -0,0 +1,25 @@
+def part1():
+    counter = 0
+    with open("input.txt") as file:
+        lines = file.readlines()
+        measurements = [int(line) for line in lines]
+        for i in range(1, len(measurements)):
+            if (measurements[i] > measurements[i - 1]):
+                counter += 1
+    print(f"{counter}")
+
+def part2():
+    counter = 0
+    with open("input.txt") as file:
+        lines = file.readlines()
+        measurements = [int(line) for line in lines]
+        for i in range(0, len(measurements) - 3):
+            first_window = measurements[i] + measurements[i + 1] + measurements[i + 2]
+            second_window = measurements[i + 1] + measurements[i + 2] + measurements[i + 3]
+            if (second_window > first_window):
+                counter += 1
+    print(f"{counter}")
+
+if __name__ == "__main__":
+    part1()
+    part2()
