389411fb8f07c58bf73a1186f078ee40f47f321b
richwill28 <e0550368@u.nus.edu>
2021-12-04 00:57:20 +0800

    Style(Day 1): Rearrange python code

--- Day 1/python/main.py
+++ Day 1/python/main.py
@@ -1,23 +1,23 @@
 def part_one():
-    counter = 0
     with open("../input.txt") as file:
         lines = file.readlines()
-        measurements = [int(line) for line in lines]
-        for i in range(1, len(measurements)):
-            if (measurements[i] > measurements[i - 1]):
-                counter += 1
+    counter = 0
+    measurements = [int(line) for line in lines]
+    for i in range(1, len(measurements)):
+        if measurements[i] > measurements[i - 1]:
+            counter += 1
     print(counter)
 
 def part_two():
-    counter = 0
     with open("../input.txt") as file:
         lines = file.readlines()
-        measurements = [int(line) for line in lines]
-        for i in range(0, len(measurements) - 3):
-            first_window = measurements[i] + measurements[i + 1] + measurements[i + 2]
-            second_window = measurements[i + 1] + measurements[i + 2] + measurements[i + 3]
-            if (second_window > first_window):
-                counter += 1
+    counter = 0
+    measurements = [int(line) for line in lines]
+    for i in range(0, len(measurements) - 3):
+        first_window = measurements[i] + measurements[i + 1] + measurements[i + 2]
+        second_window = measurements[i + 1] + measurements[i + 2] + measurements[i + 3]
+        if second_window > first_window:
+            counter += 1
     print(counter)
 
 if __name__ == "__main__":
